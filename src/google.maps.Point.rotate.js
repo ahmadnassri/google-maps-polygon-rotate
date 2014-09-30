d724fd5068f05bdc548ecca6ae7f6a3272fbcb9c
Ahmad Nassri <ahmad@codeinchaos.com>
2014-09-29 19:18:26 -0700

    fixed wrong doc header

--- src/google.maps.Point.rotate.js
+++ src/google.maps.Point.rotate.js
@@ -1,16 +1,19 @@
 /**
- * google.maps.Polygon.rotate() - Calculates a polygon's center point
+ * google.maps.Polygon.rotate() - Rotates a google.maps.Point relative to another google.maps.Point
  * @author Ahmad Nassri <ahmad@ahmadnassri.com> (http:/www.ahmadnassri.com/)
  * @repository https://github.com/ahmadnassri/google-maps-rotate
  * @license MIT
  *
- * @method getCenter
+ * @method rotate
  *
- * @return {Object} A google.maps.LatLng object
+ * @param {Number} angle Degrees to rotate
+ * @param {Object} point A google.maps.Point object to rotate relative to
+ *
+ * @return {Number} Returns A google.maps.Point with new coordinates
  */
 
 google.maps.Point.prototype.rotate = function (angle, origin) {
-  var rad = angle * (Math.PI / 180.0);
+  var rad = angle * (Math.PI / 180);
 
   var x = this.x - origin.x;
   var y = this.y - origin.y;
